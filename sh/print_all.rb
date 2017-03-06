70444a42d093c9072c640f84f2d399506feae41c
Roman Heinrich <roman.heinrich@gmail.com>
2017-03-06 14:12:57 +0100

    include urls in readme

--- sh/print_all.rb
+++ sh/print_all.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 require "fileutils"
+urls = []
 Dir.glob("src/*").each do |f|
   FileUtils.cd(f) do
     content = File.read(".git/config")
     url = content.split("\n").grep(/url/).first
-    puts url.split("= ").last
+    urls << url.split("= ").last
   end
 end
+
+puts urls.map{|x| "- #{x}"}
